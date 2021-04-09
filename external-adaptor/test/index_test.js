47728f2a32d36ad952b1aa8ff191150f2bb53ea8
Yu Yang <f14r33@gmail.com>
2021-04-09 15:15:03 -0400

    adaptor

---
+++ external-adaptor/test/index_test.js
@@ -0,0 +1,47 @@
+const assert = require('chai').assert
+const createRequest = require('../index.js').createRequest
+
+describe('createRequest', () => {
+  const jobID = '1'
+
+  context('successful calls', () => {
+    const requests = [
+      { name: 'id not supplied', testData: { data: { image_url: 'https://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg' } } },
+      { name: 'base/quote', testData: { id: jobID, data: { image_url: 'https://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg' } } },
+      { name: 'image_url/picture', testData: { id: jobID, data: { from: '' } } },
+    ]
+
+    requests.forEach(req => {
+      it(`${req.name}`, (done) => {
+        createRequest(req.testData, (statusCode, data) => {
+          assert.equal(statusCode, 200)
+          assert.equal(data.jobRunID, jobID)
+          assert.isNotEmpty(data.data)
+          assert.isAbove(Number(data.result), 0)
+          assert.isAbove(Number(data.data.result), 0)
+          done()
+        })
+      })
+    })
+  })
+
+  context('error calls', () => {
+    const requests = [
+      { name: 'empty body', testData: {} },
+      { name: 'empty data', testData: { data: {} } },
+      { name: 'unknown image', testData: { id: jobID, data: { image_url: 'not_real' } } }
+    ]
+
+    requests.forEach(req => {
+      it(`${req.name}`, (done) => {
+        createRequest(req.testData, (statusCode, data) => {
+          assert.equal(statusCode, 500)
+          assert.equal(data.jobRunID, jobID)
+          assert.equal(data.status, 'errored')
+          assert.isNotEmpty(data.error)
+          done()
+        })
+      })
+    })
+  })
+})
